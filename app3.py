16f18645b1595243a457ff3cb6e684e309833825
mnkeing-marlon <mnkeing@gmail.com>
2025-09-10 11:40:26 +0100

    app3.py

--- app3.py
+++ app3.py
@@ -60,11 +60,12 @@ l.metric(label="Balance ⚖",value= f" ${round(balance,2)}",delta=round(balance/
 col1,col2 = st.columns(2)
 with col1:
     fig1=plt.figure(figsize=(9,8))
-    sns.barplot(data=data,y="age",x="marital")
+    sns.barplot(data=data,y="age",x="marital",palette="pastel")
     st.pyplot(fig1)
 with col2:
     fig2=plt.figure(figsize=(9,8))
-    sns.histplot(data=data,x="age")
+    sns.histplot(data=data,x="age",palette="pastel")
     st.pyplot(fig2)
+
 
 
